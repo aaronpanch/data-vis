b0e0a9d43a7f56f24748703840227850390afc1d
Aaron Panchal <aaron.panchal@gmail.com>
2019-02-01 10:17:45 -0800

    Provide data to bubble plot from page

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,17 @@ import Layout from "../components/layout";
 import BubbleScatter from "../components/bubble-scatter";
 
 export default class extends React.Component {
+  static async getInitialProps() {
+    const data = await import("../static/data.json");
+    return { data };
+  }
+
   render() {
+    const { data } = this.props;
+
     return (
       <Layout>
-        <BubbleScatter width={1000} height={840} />
+        <BubbleScatter width={1000} height={840} data={data.default} />
       </Layout>
     );
   }
