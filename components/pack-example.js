7fe204071702921fc6bfc3df962cd374f19adde3
Aaron Panchal <aaron.panchal@gmail.com>
2019-01-31 22:47:56 -0800

    remove Pack-ing

--- components/pack-example.js
+++ components/pack-example.js
@@ -1,7 +1,5 @@
 import { Zoom } from "@vx/zoom";
 import { Group } from "@vx/group";
-import { Pack } from "@vx/hierarchy";
-import { hierarchy } from "d3-hierarchy";
 import { scaleQuantize } from "@vx/scale";
 import { exoplanets as data } from "@vx/mock-data";
 import { Spring } from "react-spring";
@@ -20,78 +18,63 @@ const colorScale = scaleQuantize({
   range: ["#ffe108", "#ffc10e", "#fd6d6f", "#855af2", "#11d2f9", "#49f4e7"]
 });
 
-export default ({
-  width,
-  height,
-  margin = {
-    top: 10,
-    left: 30,
-    right: 40,
-    bottom: 80
-  }
-}) => {
-  const data = hierarchy(pack)
-    .sum(d => d.radius * d.radius)
-    .sort((a, b) => {
-      return (
-        !a.children - !b.children ||
-        isNaN(a.data.distance) - isNaN(b.data.distance) ||
-        a.data.distance - b.data.distance
-      );
-    });
+export default ({ width, height }) => {
+  const circles = [
+    {
+      r: 50,
+      x: 300,
+      y: 300,
+      data: {
+        radius: 1000
+      }
+    }
+  ];
 
   return (
     <svg width={width} height={height}>
-      <Pack root={data} size={[width, height]}>
-        {pack => {
-          const circles = pack.descendants().slice(2);
+      <Zoom
+        width={width}
+        height={height}
+        scaleXMin={1 / 2}
+        scaleXMax={10}
+        scaleYMin={1 / 2}
+        scaleYMax={10}
+      >
+        {zoom => {
           return (
-            <Zoom
-              width={width}
-              height={height}
-              scaleXMin={1 / 2}
-              scaleXMax={10}
-              scaleYMin={1 / 2}
-              scaleYMax={10}
-            >
-              {zoom => {
+            <Spring to={{ matrix: zoom.toString() }}>
+              {({ matrix }) => {
                 return (
-                  <Spring to={{ matrix: zoom.toString() }}>
-                    {({ matrix }) => {
+                  <Group transform={matrix}>
+                    {circles.map((circle, i) => {
                       return (
-                        <Group transform={matrix}>
-                          {circles.map((circle, i) => {
-                            return (
-                              <circle
-                                key={`cir-${i}`}
-                                r={circle.r}
-                                cx={circle.x}
-                                cy={circle.y}
-                                fill={colorScale(circle.data.radius)}
-                                onClick={event => {
-                                  const center = {
-                                    x: width / 2,
-                                    y: height / 2
-                                  };
+                        <circle
+                          key={`cir-${i}`}
+                          r={circle.r}
+                          cx={circle.x}
+                          cy={circle.y}
+                          fill={colorScale(circle.data.radius)}
+                          onClick={event => {
+                            const center = {
+                              x: width / 2,
+                              y: height / 2
+                            };
 
-                                  zoom.setTranslate({
-                                    translateX: center.x - circle.x,
-                                    translateY: center.y - circle.y
-                                  });
-                                }}
-                              />
-                            );
-                          })}
-                        </Group>
+                            zoom.setTranslate({
+                              translateX: center.x - circle.x,
+                              translateY: center.y - circle.y
+                            });
+                          }}
+                        />
                       );
-                    }}
-                  </Spring>
+                    })}
+                  </Group>
                 );
               }}
-            </Zoom>
+            </Spring>
           );
         }}
-      </Pack>
+      </Zoom>
     </svg>
   );
 };
