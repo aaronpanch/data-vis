af6b31623b0c737c3bc3d24102599232a909ceaa
Aaron Panchal <aaron.panchal@gmail.com>
2019-02-01 10:14:02 -0800

    convert index page to React component class

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import React from "react";
 import Layout from "../components/layout";
 import BubbleScatter from "../components/bubble-scatter";
 
-export default () => (
-  <Layout>
-    <BubbleScatter width={1000} height={840} />
-  </Layout>
-);
+export default class extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <BubbleScatter width={1000} height={840} />
+      </Layout>
+    );
+  }
+}
