32f44619046330f3c12c53a155835c5fc8133489
Aaron Panchal <aaron.panchal@gmail.com>
2019-02-02 14:31:13 -0800

    get center and zoom to work

--- components/bubble-scatter.js
+++ components/bubble-scatter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Zoom } from "@vx/zoom";
+import { Zoom, composeMatrices, translateMatrix, scaleMatrix } from "@vx/zoom";
 import { scaleLinear, scaleLog, scaleQuantize } from "@vx/scale";
 import { Spring, animated } from "react-spring";
 
@@ -8,92 +8,101 @@ const extent = (data, value = d => d) => [
   Math.max(...data.map(value))
 ];
 
-export default ({ width, height, data, margin = 30 }) => {
-  const colorScale = scaleQuantize({
-    domain: extent(data, d => d.freq),
-    range: ["#855af2", "#11d2f9", "#49f4e7"]
-  });
-
-  const xScale = scaleLinear({
-    domain: extent(data, d => d.x),
-    range: [margin, width - margin]
-  });
-
-  const yScale = scaleLinear({
-    domain: extent(data, d => d.y),
-    range: [margin, height - margin]
-  });
-
-  const rScale = scaleLinear({
-    domain: extent(data, d => Math.sqrt(d.freq)),
-    range: [5, 100]
-  });
-
-  const center = {
-    x: width / 2,
-    y: height / 2
-  };
-
-  const centerOn = (zoom, { x, y, z = 1 }) => {
-    zoom.setTransformMatrix({
-      scaleX: z,
-      scaleY: z,
-      translateX: center.x - x,
-      translateY: center.y - y,
-      skewX: 0,
-      skewY: 0
+export default class extends React.Component {
+  render() {
+    const { width, height, data, margin = 30 } = this.props;
+    const colorScale = scaleQuantize({
+      domain: extent(data, d => d.freq),
+      range: ["#855af2", "#11d2f9", "#49f4e7"]
     });
-  };
 
-  return (
-    <svg width={width} height={height}>
-      <Zoom
-        width={width}
-        height={height}
-        scaleXMin={1 / 2}
-        scaleXMax={10}
-        scaleYMin={1 / 2}
-        scaleYMax={10}
-      >
-        {zoom => {
-          return (
-            <React.Fragment>
-              <rect
-                width={width}
-                height={height}
-                fill="#fff"
-                onClick={() => centerOn(zoom, center)}
-              />
-              <Spring native to={{ matrix: zoom.toString() }}>
-                {({ matrix }) => {
-                  return (
-                    <animated.g className="vx-group" transform={matrix}>
-                      {data.map((topic, i) => {
-                        return (
-                          <circle
-                            key={`cir-${i}`}
-                            r={rScale(Math.sqrt(topic.freq))}
-                            cx={xScale(topic.x)}
-                            cy={yScale(topic.y)}
-                            opacity="0.7"
-                            fill={colorScale(topic.freq)}
-                            onClick={event => {
-                              centerOn(zoom, {
-                                x: xScale(topic.x),
-                                y: yScale(topic.y)
-                              });
-                            }}
-                          />
-                        );
-                      })}
-                    </animated.g>
-                  );
-                }}
-              </Spring>
-            </React.Fragment>
-          );
-        }}
-      </Zoom>
-    </svg>
-  );
-};
+    const xScale = scaleLinear({
+      domain: extent(data, d => d.x),
+      range: [margin, width - margin]
+    });
+
+    const yScale = scaleLinear({
+      domain: extent(data, d => d.y),
+      range: [margin, height - margin]
+    });
+
+    const rScale = scaleLinear({
+      domain: extent(data, d => Math.sqrt(d.freq)),
+      range: [5, 100]
+    });
+
+    const center = {
+      x: width / 2,
+      y: height / 2
+    };
+
+    const centerOn = (zoom, { x, y, z = 1 }) => {
+      zoom.setTransformMatrix(
+        composeMatrices(
+          translateMatrix(center.x, center.y),
+          scaleMatrix(z, z),
+          translateMatrix(-x, -y)
+        )
+      );
+    };
+
+    return (
+      <svg width={width} height={height}>
+        <Zoom
+          width={width}
+          height={height}
+          scaleXMin={1 / 2}
+          scaleXMax={10}
+          scaleYMin={1 / 2}
+          scaleYMax={10}
+        >
+          {zoom => {
+            return (
+              <React.Fragment>
+                <rect
+                  width={width}
+                  height={height}
+                  fill="#fff"
+                  onClick={() => centerOn(zoom, center)}
+                />
+                <Spring native to={{ matrix: zoom.toString() }}>
+                  {({ matrix }) => {
+                    return (
+                      <animated.g className="vx-group" transform={matrix}>
+                        {data.map((topic, i) => {
+                          return (
+                            <circle
+                              key={`cir-${i}`}
+                              r={rScale(Math.sqrt(topic.freq))}
+                              cx={xScale(topic.x)}
+                              cy={yScale(topic.y)}
+                              opacity="0.7"
+                              fill={colorScale(topic.freq)}
+                              onClick={event => {
+                                centerOn(zoom, {
+                                  x: xScale(topic.x),
+                                  y: yScale(topic.y),
+                                  z: Math.min(
+                                    5,
+                                    (width - 50) /
+                                      (2 * rScale(Math.sqrt(topic.freq))),
+                                    (height - 50) /
+                                      (2 * rScale(Math.sqrt(topic.freq)))
+                                  )
+                                });
+                              }}
+                            />
+                          );
+                        })}
+                      </animated.g>
+                    );
+                  }}
+                </Spring>
+              </React.Fragment>
+            );
+          }}
+        </Zoom>
+      </svg>
+    );
+  }
+}
